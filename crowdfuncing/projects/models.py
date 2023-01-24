e9b0fa357b2dcbfcd21c605dc6235d60f21d7842
yomandy <yomandy@users.noreply.github.com>
2023-01-24 18:42:25 +1100

    first main commit

---
+++ crowdfuncing/projects/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField()
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owner_projects'
+        )
+
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='supporter_pledges'
+    )
+
